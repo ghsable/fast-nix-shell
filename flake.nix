b1071f10cb87057c98b6e7cb862935cd2995c732
dativus <sn.sable005@gmail.com>
2022-07-23 23:59:51 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "My Project";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
